f78ec7e240a7b10dfe623d6847ab3408958b9642
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-26 03:24:13 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,5 +1,5 @@
 def count_strings(array)
-  array.count(String)
+  array.count { |element| element=String}
   
   # Return the total number of strings in the provided array using the count enumerable
 end
