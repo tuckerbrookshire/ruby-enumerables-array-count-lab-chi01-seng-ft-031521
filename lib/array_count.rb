5d6fd4654d8b37002082f80a27c6c5af7a443df9
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-26 04:19:16 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -7,7 +7,7 @@ end
 
 def count_empty_strings(array)
   array.count do |element|
-    element = ""
+    element = ("")
   end
   # Return the total number of EMPTY strings in the provided array using the count enumerable
 end
