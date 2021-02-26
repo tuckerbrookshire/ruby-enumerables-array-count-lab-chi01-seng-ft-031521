d7dbac9d04fe7acdc936a61319b8d1240007800a
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-26 04:08:24 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,6 +1,6 @@
 def count_strings(array)
   array.count do |element|
-    element == String
+    element.class == String
   
   # Return the total number of strings in the provided array using the count enumerable
 end
