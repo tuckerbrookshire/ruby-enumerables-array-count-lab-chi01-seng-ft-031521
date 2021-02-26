b9ef0ebba18b7644f9f8d9dd948f9bf104d79c09
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-26 04:19:36 +0000

    Done.

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,13 @@
 def count_strings(array)
+  array.count do |element|
+    element.class == String
+  end
   # Return the total number of strings in the provided array using the count enumerable
 end
 
 def count_empty_strings(array)
+  array.count do |element|
+    element == ("")
+  end
   # Return the total number of EMPTY strings in the provided array using the count enumerable
 end
